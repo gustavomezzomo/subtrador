35aebf6f4af7b6fcdcbc8efefb595495e1f766de
Gustavo Mezzomo <77406018+gustavomezzomo@users.noreply.github.com>
2022-09-02 10:20:03 -0300

    Delete codigo directory

--- codigo/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Subtrador {
-    subtrair(numero1, numero2) {
-        return numero1 - numero2;
-    }
-}
-exports.default = Subtrador;
--- codigo/index.ts
+++
@@ -1,5 +0,0 @@
-export default class Subtrador{
-    subtrair(numero1: number, numero2: number){
-        return numero1 - numero2
-    }
-}
